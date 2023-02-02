764866344aa74c860837ab732921092df3fd1e05
Vasjan Çupri <102856709+v4sj4n@users.noreply.github.com>
2023-02-02 21:05:21 +0100

    added new colors

--- Program.cs
+++ Program.cs
@@ -5,28 +5,47 @@ namespace proj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("g = green, b = blue, w = white, r = red : ");
+            Console.WriteLine("g = green, b = blue, w = white, r = red, c = cyan , m = magenta : ");
             int x = Console.Read();
-            switch (Convert.ToChar(x))
+            char userInput = Convert.ToChar(x);
+            while (userInput != 'x')
             {
-                case 'g':
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    System.Console.WriteLine("You clicked green");
-                    break;
-                case 'b':
-                    Console.BackgroundColor = ConsoleColor.Blue;
-                    System.Console.WriteLine("You clicked blue");
-                    break;
-                case 'w':
-                    Console.BackgroundColor = ConsoleColor.White;
-                    System.Console.WriteLine("You clicked white");
-                    break;
-                case 'r':
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    System.Console.WriteLine("You clicked white");
-                    break;
-                default:
-                    break;
+
+                switch (Convert.ToChar(x))
+                {
+                    case 'g':
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        System.Console.WriteLine("You clicked green");
+                        break;
+                    case 'c':
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        System.Console.WriteLine("You clicked cyan");
+                        break;
+                    case 'b':
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        System.Console.WriteLine("You clicked blue");
+                        break;
+                    case 'm':
+                        Console.BackgroundColor = ConsoleColor.Magenta;
+                        System.Console.WriteLine("You clicked blue");
+                        break;
+                    case 'w':
+                        Console.BackgroundColor = ConsoleColor.White;
+                        System.Console.WriteLine("You clicked white");
+                        break;
+                    case 'r':
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        System.Console.WriteLine("You clicked white");
+                        break;
+                    default:
+                        break;
+                }
+                Console.Clear();
+                Console.WriteLine("g = green, b = blue, w = white, r = red, c = cyan , m = magenta : ");
+
+                x = Console.Read();
+
+
             }
         }
     }
