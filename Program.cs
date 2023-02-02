cde83176313e9fba2de2f2939f7afd0f55d8b859
Vasjan Çupri <102856709+v4sj4n@users.noreply.github.com>
2023-02-02 20:58:13 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("g = green, b = blue, w = white, r = red : ");
+            int x = Console.Read();
+            switch (Convert.ToChar(x))
+            {
+                case 'g':
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    System.Console.WriteLine("You clicked green");
+                    break;
+                case 'b':
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    System.Console.WriteLine("You clicked blue");
+                    break;
+                case 'w':
+                    Console.BackgroundColor = ConsoleColor.White;
+                    System.Console.WriteLine("You clicked white");
+                    break;
+                case 'r':
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    System.Console.WriteLine("You clicked white");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
